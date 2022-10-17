6c8f6c9672cd419b1588df87301bd80209b27b3a
arunnarangali <arunnarangali123@gmail.com>
2022-10-17 21:32:03 +0530

    firstcomit

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+$('#menu').click(function(){
+ $(this).toggleClass('fa-times');
+ $('header').toggleClass('toggle');
+
+});
+
+$(Window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+    
+    if($(window).scrollTop() > 0){
+        $('.top').show();
+    }else{
+        $('.top').hide();
+    }
+
+});
+
+$
+
+})
